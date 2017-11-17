15833c8bde5b1f05b1326000c062738715b185b0
Cristian Arrieta <cristian@150porciento.com>
2017-11-17 18:28:04 -0500

    Get service

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const async = require('async');
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/upcoming', (req, res) => {
     data = [
-        {id:"1",name:"Boeign"},
-        {id:"2",name:"Airbus"}        
+        { id: "1", name: "Boeign" },
+        { id: "2", name: "Airbus" }
     ]
     res.json(data);
     // async.times(2, (i, callback) => {
