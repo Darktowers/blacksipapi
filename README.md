2d46780318caca6c5cd2ee01929f145a723eb381
Cristian <cristian@150porciento.com>
2017-11-18 01:08:44 -0500

    Ajuste POST

---
+++ README.md
@@ -0,0 +1,9 @@
+# BlacksipAPI Test Cristian Arrieta
+
+Para probar el api
+
+- Correr npm install
+- tener instalado NodeJS
+- correr el comando node index.js sobre el directorio
+- EL API correra sobre el puerto 8010
+
--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const async = require('async');
-app.use(function (req, res, next) {
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
@@ -20,31 +25,21 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-app.get('/upcoming', (req, res) => {
+app.get('/aviones', (req, res) => {
     data = [
         { id: "1", name: "Boeign" },
         { id: "2", name: "Airbus" }
     ]
     res.json(data);
-    // async.times(2, (i, callback) => {
-    // var options = {
-    //     url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=API_KEY',
-    //     qs: {
-    //         'language': 'en-US',
-    //         'page': i+1,
-    //         'region': 'us'
-    //     },
-    // } 
-    // request(options, (error, response, body) => {
-    //     var result = JSON.parse(body)
-    //     var data = result.results;
-    //     callback(null, data);
-    // });
-    // }, (err, results) => {
-    //     res.json(results);
-    // });
 })
-
-app.listen('8010', () => {
+app.post("/sms", function (req, res) {
+    console.log(req.body);
+    res.send({ status: true, body: req.body });
+});
+app.listen('8010', (err) => {
+    if (err) {
+        throw err
+    }
     console.log('Listening on port 8010');
 })
+
